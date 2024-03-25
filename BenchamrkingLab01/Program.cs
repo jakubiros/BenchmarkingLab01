bb0a3655edbddac8ddce50016f642ec876e2c353
jakubiros <jakub.biros@microsoft.wsei.edu.pl>
2024-03-25 22:17:47 +0100

    Added InsertionSort and created first benchmark

--- BenchamrkingLab01/Program.cs
+++ BenchamrkingLab01/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace BenchmarkingLab01
+﻿using BenchmarkDotNet.Running;
+
+namespace BenchmarkingLab01
 {
     public class Program
     {
         static void Main(string[] args)
         {
-
+            var summary = BenchmarkRunner.Run(typeof(SortingAlgorithms));
         }
     }
 }
--- BenchamrkingLab01/SortingAlgorithms.cs
+++ BenchamrkingLab01/SortingAlgorithms.cs
@@ -1,13 +1,56 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BenchmarkingLab01
 {
+    [MemoryDiagnoser]
     public class SortingAlgorithms
     {
+        private Dictionary<string, int[]> arrays;
 
+        public SortingAlgorithms()
+        {
+            arrays = new Dictionary<string, int[]>
+            {
+                { "Random", Generators.GenerateRandom(10,1,100) },
+                { "Sorted", Generators.GenerateSorted(10,1,100) },
+                { "Reversed", Generators.GenerateReversed(10,1,100) },
+                { "AlmostSorted", Generators.AlmostSorted(10,1,100) },
+                { "Few Unique", Generators.FewUnique(10) }
+            };
+        }
+
+        public IEnumerable<string> GetArrayNames()
+        {
+            return arrays.Keys;
+        }
+
+        [ParamsSource(nameof(GetArrayNames))]
+        public string ArrayName { get; set; }
+
+        [Benchmark]
+        public int[] InsertionSort()
+        {
+            int[] arrClone = (int[])arrays[ArrayName].Clone();
+            int n = arrClone.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arrClone[i];
+                int j = i - 1;
+
+                while (j >= 0 && arrClone[j] > key)
+                {
+                    arrClone[j + 1] = arrClone[j];
+                    j = j - 1;
+                }
+                arrClone[j + 1] = key;
+            }
+            return arrClone;
+        }
     }
 }
